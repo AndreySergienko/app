541ef3425a8d4724ccac51357811653230ec8460
Andrey Sergineko <getmessage@gmail.com>
2024-09-13 11:25:11 +0500

    eslint&prettier install

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import eslintConfigPrettier from 'eslint-config-prettier'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginJs from '@eslint/js'
+import pluginVue from 'eslint-plugin-vue'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+/** Vue/Nuxt rules */
+const vueRules = {
+  'vue/max-attributes-per-line': [
+    'error',
+    {
+      singleline: {
+        max: 4
+      },
+      multiline: {
+        max: 4
+      }
+    }
+  ]
+}
+
+export default withNuxt(
+  /** Compiler options */
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  /** Plugins */
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  eslintConfigPrettier,
+  /** Rules */
+  {
+    rules: {
+      ...vueRules,
+
+      /** Required rules */
+      'comma-dangle': ['error', 'never'],
+      semi: ['error', 'never'],
+      'no-trailing-spaces': ['error'],
+      'eol-last': ['error', 'always'],
+      'max-lines': ['error', 175],
+      'max-len': ['error', 100]
+    }
+  },
+  /** Ignores */
+  {
+    ignores: ['node_modules', 'dist', 'public', '.nuxt']
+  }
+)
