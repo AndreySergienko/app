f62249227c0cd9b25e6e66dd1f0c1a594bffee25
a.sergienko <a.sergienko@inetstudio.ru>
2024-10-09 00:03:27 +0500

    fix: update linters

---
+++ stylelint.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('stylelint').Config} */
+
+export default {
+  customSyntax: 'postcss-html',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-idiomatic-order'
+  ],
+  plugins: ['stylelint-order'],
+  rules: {
+    /** Regular expression, valid example class: .row .row-block .row-block__element .row-block__element--modificator */
+    'selector-class-pattern': [
+      '.[a-zA-Z0-9_-]+(__[a-zA-Z0-9_-]+)*',
+      { resolveNestedSelectors: true }
+    ],
+    /** Ignore vue pseudoclass */
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global', 'deep']
+      }
+    ]
+  }
+}
