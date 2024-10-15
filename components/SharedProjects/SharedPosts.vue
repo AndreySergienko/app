19585031d3d7a4e3866202087ac7948732b187f5
Sunnalen <sunnalen@mail.ru>
2024-10-15 19:27:19 +0500

    feat: SharedPosts

---
+++ components/SharedProjects/SharedPosts.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="posts">
+    <div v-for="(post, index) in posts" :key="index" class="post">
+      <img v-if="post.img" :src="post.img" alt="Post" />
+      <div class="post__inner">
+        <p class="post__date">{{ post.date }}</p>
+        <h3 class="post__title">{{ post.title }}</h3>
+        <p class="post__text">{{ post.text }}</p>
+        <a :href="post.link" class="post__link">Read more</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+import { reactive } from 'vue'
+import Post from '@/assets/images/Post.png'
+
+const posts = reactive([
+  {
+    img: Post,
+    title: 'Exploring the Trails: A Guide to Hiking Adventures',
+    text: "Nature's mountains stand as majestic monuments, towering over the landscapes\
+            that stretch far and wide. Hiking through these natural wonders offers an escape to\
+            the serene and untouched corners of the earth. Each step taken on a mountain trail\
+            is a step closer to discovering the breathtaking beauty of the wild. Surrounded by\
+            the fresh, crisp air and the tranquil sounds of nature, hikers find a unique peace\
+            and a profound connection to the earth. Mountains challenge and inspire, urging us\
+            to explore further, climb higher, and appreciate the natural world in its grandest\
+            form...",
+    date: 'September 17, 2024',
+    link: '#'
+  },
+  {
+    img: '',
+    title: 'ON-DEVELOPER: New Ad Platform to Fulfill Your Needs',
+    text: "I was faced with the same dilemma a few months ago. I finally had the motivation\
+            to create a new personal website, but wasn't sure where to start. I decided\
+            to keep things simple, I'd write it with a library I knew very well: React.",
+    date: 'September 17, 2024',
+    link: '#'
+  }
+])
+</script>
+<style>
+.posts {
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap-m);
+}
+
+.post {
+  background-color: var(--gray);
+}
+
+.post__inner {
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  gap: var(--gap-l);
+}
+
+.post__date,
+.post__link {
+  font-size: var(--font-size-s);
+  font-weight: var(--weight-medium);
+  color: var(--dark-gray);
+}
+
+.post__title {
+  font-size: var(--font-size-m);
+  font-weight: var(--weight-medium);
+  color: var(--dark);
+}
+
+.post__text {
+  font-size: var(--font-size-s);
+  font-weight: var(--weight-regular);
+  color: var(--dark);
+  line-height: 18px;
+}
+
+.post__link {
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+.post__link:hover {
+  color: var(--dark);
+}
+</style>
--- components/SharedTab/SharedTab.vue
+++ components/SharedTab/SharedTab.vue
@@ -11,32 +11,8 @@
       </div>
     </div>
     <div class="tab__content">
-      <div v-if="activeTab === 'Posts'" class="tab__content-items">
-        <div class="tab__content-item">
-          <img class="tab__content-img" src="~/assets/images/Post.png" alt="Post">
-          <div class="tab__content-inner">
-            <div class="tab__content-date">September 17, 2024</div>
-            <div class="tab__content-title">Exploring the Trails: A Guide to Hiking Adventures</div>
-            <div class="tab__content-text">Nature's mountains stand as majestic monuments, towering over the landscapes that stretch far and wide. 
-              Hiking through these natural wonders offers an escape to the serene and untouched corners of the earth. 
-              Each step taken on a mountain trail is a step closer to discovering the breathtaking beauty of the wild. 
-              Surrounded by the fresh, crisp air and the tranquil sounds of nature, hikers find a unique peace and a profound connection to the earth. 
-              Mountains challenge and inspire, urging us to explore further, climb higher, and appreciate the natural world in its grandest form...
-            </div>
-            <a href="#" class="tab__content-link">Read more</a>
-          </div>
-        </div>
-        <div class="tab__content-item">
-          <div class="tab__content-inner">
-            <div class="tab__content-date">September 17, 2024</div>
-            <div class="tab__content-title">ON-DEVELOPER: New Ad Platform to Fulfill Your Needs</div>
-            <div class="tab__content-text"> I was faced with the same dilemma a few months ago.
-              I finally had the motivation to create a new personal website, but wasn't sure where to start.
-              I decided to keep things simple, I'd write it with a library I knew very well: React.
-            </div>
-            <a href="#" class="tab__content-link">Read more</a>
-          </div>
-        </div>
+      <div v-if="activeTab === 'Posts'">
+        <SharedPosts />
       </div>
       <div v-if="activeTab === 'Projects'">
         <div>Here is a projects...</div>
@@ -47,6 +23,7 @@
 
 <script setup>
 import { ref } from 'vue'
+import SharedPosts from '../SharedProjects/SharedPosts.vue'
 
 const tabs = ['Posts', 'Projects']
 const activeTab = ref('Posts')
@@ -75,51 +52,4 @@ const activeTab = ref('Posts')
   max-width: 670px;
   margin-top: 20px;
 }
-
-.tab__content-items {
-  display: flex;
-  flex-direction: column;
-  gap: var(--gap-m);
-}
-
-.tab__content-item {
-  background-color: var(--gray);
-}
-
-.tab__content-inner {
-  padding: 40px;
-  display: flex;
-  flex-direction: column;
-  gap: var(--gap-l);
-}
-
-.tab__content-date, .tab__content-link {
-  font-size: var(--font-size-s);
-  font-weight: var(--weight-medium);
-  color: var(--dark-gray);
-}
-
-.tab__content-title {
-  font-size: var(--font-size-m);
-  font-weight: var(--weight-medium);
-  color: var(--dark);
-}
-
-.tab__content-text {
-  font-size: var(--font-size-s);
-  font-weight: var(--weight-regular);
-  color: var(--dark);
-  line-height: 18px;
-}
-
-.tab__content-link {
-  text-decoration: none;
-  transition: color 0.3s ease;
-}
-
-.tab__content-link:hover {
-  color: var(--dark);
-}
-
 </style>
-
